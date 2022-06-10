12398cd450c152857b20210ad29c5047b8087fe0
Caritoozepeda <carolina.zepeda47@gmail.com>
2022-06-09 22:04:13 -0300

    actualizaac

---
+++ js/javas.js
@@ -0,0 +1,129 @@
+
+/*.....................*validacion*.....................*/
+
+
+function validarEmail(mail) {
+
+
+      var mail=document.datos.mail.value;
+      
+          var exReg=/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+          var esValido=exReg.test(mail);
+
+         if (!esValido){
+           alert("La dirección de email es incorrecta");
+           return false;
+           document.datos.mail.focus();
+           
+         } 
+        }
+
+function validar(){        
+      var nombre= document.datos.nombre; 
+      if(nombre.value.trim()=="" || nombre.value.trim().length>30 || (!isNaN(nombre.value.trim()))){
+            alert("Ingrese un nombre valido");
+            
+            return false;
+            nombre.focus();
+          }
+          if(confirm("Desea enviar los datos")){
+                alert("Gracias por enviar el formulario");
+
+              }else 
+              return false;
+              document.datos.reset();
+
+                 }
+/* menu*/
+
+/*let btnMenu = document.querySelector('.btn-menu');
+let barIconX = document.querySelector('.btn-menu i');
+let menu = document.querySelector('.list-container');
+let menuContent = document.querySelector('.menu');
+var activador = true;
+
+btnMenu.addEventListener('click', (event) => {
+
+  //Icon X
+  barIconX.classList.toggle('fa-times');
+
+   if(activador){
+     menu.style.left = '0%'; 
+     menu.style.transition = '0.5s';
+  
+     activador = false;
+   }
+   else{
+    activador = false;
+    menu.style.left = '-100%';
+
+    activador = true;
+   }
+
+});
+*/
+// Add class "active"
+let enlaces = document.querySelectorAll('.lists li a');
+
+enlaces.forEach((element) => {
+   
+  element.addEventListener('click', (event) => {
+   enlaces.forEach((link) => {
+     link.classList.remove('active');
+   });
+    event.target.classList.add('active');
+
+  });
+
+});
+
+//Scroll Efect
+/*
+ let prevScrollPos = window.pageYOffset;
+ let goTop = document.querySelector('.ir-arriba');
+
+window.onscroll = () => {
+  
+  //Hide & Show - Scroll Menu (Function)
+  let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    menuContent.style.top = '0px';
+    menuContent.style.transition = '0.5s';
+  }else{
+    menuContent.style.top = '-60px';
+    menuContent.style.transition = '0.5s';
+  }
+  prevScrollPos = currentScrollPos;
+  
+  //Scoll Menu & Go Top & See Down (Styles)
+  let arriba = window.pageYOffset;
+
+  //Conditions
+  if(arriba <= 600){
+    menuContent.style.borderBottom = 'none';
+
+    //Ocultar Go Top
+    goTop.style.right = '-100px';
+  }else{
+    menuContent.style.borderBottom = '3px solid #ff2e63';
+
+    //Mostrar Go Top
+    goTop.style.right = '0px';
+  }
+  
+}
+
+//Go Top Click
+goTop.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+let abajo = document.querySelector('#abajo');
+
+abajo.addEventListener('click', () => {
+  document.body.scrollTop = 600;
+  document.documentElement.scrollTop = 600;
+  
+});*/
